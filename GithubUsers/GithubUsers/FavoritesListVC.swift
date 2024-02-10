cca5ddbcb79e830f7406b89422e60e944c361774
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-02-09 20:16:56 -0500

    Added Project and Implemented Controller Hierarchy

---
+++ GithubUsers/GithubUsers/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GithubUsers
+//
+//  Created by Marco Capraro on 2/9/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GithubUsers/GithubUsers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GithubUsers
+//
+//  Created by Marco Capraro on 2/9/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+}
