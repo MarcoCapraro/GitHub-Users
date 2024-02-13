792054c7971919e2fdec5824bf8e960495f0b5ef
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-02-13 17:16:29 -0500

    Added UITextFieldDelegate; Created FollowersListVC
    Added a UITextFieldDelegate extension to the SearchVC and a target to the callToActionButton that listens to username submissions and then pushes the FollowersListVC onto the stack. Listens for whenever the user either inputs a username and selects the text field return button, or inputs a username, taps outside the text field view to dismiss it and then taps the call to action button "Get Followers". The username entered gets passed as data to the FollowersListVC.

--- GithubUsers/GithubUsers/GUTextField.swift
+++ GithubUsers/GithubUsers/GUTextField.swift
@@ -37,7 +37,7 @@ class GUTextField: UITextField {
         
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
-        
+        returnKeyType = .go
         placeholder = "Enter a username"
     }
 
