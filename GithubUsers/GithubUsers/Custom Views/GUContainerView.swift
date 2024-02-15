5689ed39fd0bbea9946615daa3edc5b290355307
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-02-15 15:30:58 -0500

    Project Organization
    Added Folders, and moved files into the newly create folders for a more organized project structure.

---
+++ GithubUsers/GithubUsers/Custom Views/GUContainerView.swift
@@ -0,0 +1,32 @@
+//
+//  GUContainerView.swift
+//  GithubUsers
+//
+//  Created by Marco Capraro on 2/15/24.
+//
+
+import UIKit
+
+class GUContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Custom Configuration
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 15
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+}
