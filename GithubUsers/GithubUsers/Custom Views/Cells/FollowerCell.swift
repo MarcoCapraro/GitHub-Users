d45991f3edeb8b3f59683708bd5edec738dba9b7
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-02-27 14:45:51 -0500

    Created FollowerCell and GUAvatarImageView for UICollectionVIew
    When the network manager retrieves a list of followers, our codebase should then display a UICollectionView with a cell for each follower. I created the custom Follower cell UICollectionViewCell and and configured it to contain an avatar image and a username label.

---
+++ GithubUsers/GithubUsers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,52 @@
+//
+//  FollowerCell.swift
+//  GithubUsers
+//
+//  Created by Marco Capraro on 2/27/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    
+    let avatarImageView = GUAvatarImageView(frame: .zero)
+    let usernameLabel = GUTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Configure Custom Views Here
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        NSLayoutConstraint.activate([
+            // Avatar Image View Constraints
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            // Ensure square avatar image view
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            // Username Label Constraints
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
