2fffc1184a5d804c9e7b0df775b973f11fafa40c
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-03-06 17:02:43 -0500

    Created GUItemInfoView and GUItemInfoVC Superclass
    GUItemInfoVC is a Generic Superclass for a reusable container that contains a stack view with GUItemInfoViews and a GUButton. This will be attached to the container view created in UserInfoVC.

---
+++ GithubUsers/GithubUsers/Custom Views/ViewControllers/GUItemInfoVC.swift
@@ -0,0 +1,74 @@
+//
+//  GUItemInfoVC.swift
+//  GithubUsers
+//
+//  Created by Marco Capraro on 3/6/24.
+//
+
+import UIKit
+
+class GUItemInfoVC: UIViewController {
+    
+    // Stack view for both GUItemInfoViews
+    let stackView = UIStackView()
+    let itemInfoViewOne = GUItemInfoView()
+    let itemInfoViewTwo = GUItemInfoView()
+    let actionButton = GUButton()
+    
+    var user: User!
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        configureBackgroundView()
+        configureStackView()
+        layoutUI()
+    }
+    
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            // Stack view at top of GUItemInfoVC to hold itemInfoViewOne and itemInfoViewTwo
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            // Button at bottom of GUItemInfoVC
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+            
+        ])
+    }
+
+}
